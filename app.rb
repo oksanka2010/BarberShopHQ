29f13500ae43493990ad06960d76069106f4ae11
Oksana Kamenchuk <nofelet069@gmail.com>
2024-04-16 02:48:18 +0300

    Add Rakefile

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, { adapter: 'sqlite3', database: 'my_database.db' }
+set :database, { adapter: 'sqlite3', database: 'barbershop.db' }
 
 class Client < ActiveRecord::Base
 end
